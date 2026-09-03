90c2ad01e12b96b1ebc7a9a67e481c2ea7200293
NorahChan01 <missnorahchan@gmail.com>
2026-09-03 09:08:50 +0600

    Add MangaPill to the module exports

--- sources/index.js
+++ sources/index.js
@@ -1,7 +1,9 @@
 const MangaDex = require("./mangadex");
 const Asura = require("./asura");
+const MangaPill = require("./mangapill");
 
 module.exports = [
     MangaDex,
-    Asura
+    Asura,
+    MangaPill
 ];
