5b3c20b2a42ce74b8a9ae5e3f95cb6f59c7aea13
NorahChan01 <missnorahchan@gmail.com>
2026-09-04 00:51:23 +0600

    Remove unused manga sources from index.js

--- sources/index.js
+++ sources/index.js
@@ -1,21 +1,5 @@
 const MangaDex = require("./mangadex");
-const Asura = require("./asura");
-const MangaPill = require("./mangapill");
-const MangaK = require("./mangak");
-const MangaLivre = require("./mangalivre");
-const ManhuaPlus = require("./manhuaplus");
-const MangaDenizi = require("./mangadenizi");
-const NHentai = require("./nhentai");
-const Toongod = require("./toongod");
 
 module.exports = [
-    MangaDex,
-    Asura,
-    MangaPill,
-    MangaK,
-    MangaLivre,
-    ManhuaPlus,
-    MangaDenizi,
-    NHentai,
-    Toongod
+    MangaDex
 ];
