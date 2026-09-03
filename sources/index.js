f2a402c48d94634eee9b2afc000ec1436c75c7e7
NorahChan01 <missnorahchan@gmail.com>
2026-09-03 21:00:35 +0600

    Replace multiple manga sources with MangaDNA

--- sources/index.js
+++ sources/index.js
@@ -1,17 +1,5 @@
-const MangaDex = require("./mangadex");
-const Asura = require("./asura");
-const MangaPill = require("./mangapill");
-const MangaK = require("./mangak");
-const MangaLivre = require("./mangalivre");
-const ManhuaPlus = require("./manhuaplus");
-const MangaDenizi = require("./mangadenizi");
+const MangaDNA = require("./mangadna");
 
 module.exports = [
-    MangaDex,
-    Asura,
-    MangaPill,
-    MangaK,
-    MangaLivre,
-    ManhuaPlus,
-    MangaDenizi
+    MangaDNA
 ];
