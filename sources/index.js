09e2952bd40e0528a0ed6cf6f6982ccb2002bf03
NorahChan01 <missnorahchan@gmail.com>
2026-09-03 20:46:31 +0600

    Refactor source loading to direct module exports

--- sources/index.js
+++ sources/index.js
@@ -1,50 +1,17 @@
-const sourceFiles = [
-    "asura",
-    "atsumaru",
-    "flamecomics",
-    "mangaball",
-    "mangabuddy",
-    "mangadenizi",
-    "mangadex",
-    "mangahere",
-    "mangak",
-    "mangakatana",
-    "mangalivre",
-    "mangapill",
-    "mangaread",
-    "mangataro",
-    "manhuaplus",
-    "rawkuma",
-    "weebcentral"
+const MangaDex = require("./mangadex");
+const Asura = require("./asura");
+const MangaPill = require("./mangapill");
+const MangaK = require("./mangak");
+const MangaLivre = require("./mangalivre");
+const ManhuaPlus = require("./manhuaplus");
+const MangaDenizi = require("./mangadenizi");
+
+module.exports = [
+    MangaDex,
+    Asura,
+    MangaPill,
+    MangaK,
+    MangaLivre,
+    ManhuaPlus,
+    MangaDenizi
 ];
-
-const sources = [];
-
-for (const file of sourceFiles) {
-    try {
-        const source = require(`./${file}`);
-
-        if (!source || typeof source.getChapter !== "function") {
-            console.warn(
-                `[SOURCE] ${file}.js loaded but has no valid getChapter()`
-            );
-            continue;
-        }
-
-        sources.push(source);
-
-        console.log(
-            `[SOURCE] Loaded: ${source.name || file}`
-        );
-    } catch (error) {
-        console.error(
-            `[SOURCE] Failed to load ${file}.js: ${error.message}`
-        );
-    }
-}
-
-console.log(
-    `[SOURCE] ${sources.length}/${sourceFiles.length} sources loaded.`
-);
-
-module.exports = sources;
