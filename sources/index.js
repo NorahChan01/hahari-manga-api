4a21698af59138890990e4fdbba52f48540626d8
NorahChan01 <missnorahchan@gmail.com>
2026-09-04 01:24:40 +0600

    Add multiple manga sources to index.js

--- sources/index.js
+++ sources/index.js
@@ -1,5 +1,21 @@
 const MangaDex = require("./mangadex");
+const Asura = require("./asura");
+const MangaPill = require("./mangapill");
+const MangaK = require("./mangak");
+const MangaLivre = require("./mangalivre");
+const ManhuaPlus = require("./manhuaplus");
+const MangaDenizi = require("./mangadenizi");
+const NHentai = require("./nhentai");
+const Toongod = require("./toongod");
 
 module.exports = [
-    MangaDex
+    MangaDex,
+    Asura,
+    MangaPill,
+    MangaK,
+    MangaLivre,
+    ManhuaPlus,
+    MangaDenizi,
+    NHentai,
+    Toongod
 ];
