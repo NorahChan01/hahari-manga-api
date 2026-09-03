165960e8aebdd849fd7e7b476e5a93bcb148be23
NorahChan01 <missnorahchan@gmail.com>
2026-09-03 09:48:52 +0600

    Add MangaPark to the module exports

--- sources/index.js
+++ sources/index.js
@@ -1,9 +1,11 @@
 const MangaDex = require("./mangadex");
 const Asura = require("./asura");
 const MangaPill = require("./mangapill");
+const MangaPark = require("./mangapark");
 
 module.exports = [
     MangaDex,
     Asura,
-    MangaPill
+    MangaPill,
+    MangaPark
 ];
